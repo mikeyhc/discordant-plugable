41ad038ca0c2c13ec1d0a11ae7b2b5ec5e4cb1bb
mikeyhc <mikeyhc@atmosia.net>
2020-07-14 15:34:40 -0400

    minor fixes

--- src/discord_gateway.erl
+++ src/discord_gateway.erl
@@ -14,7 +14,8 @@
                  connection :: #connection{} | undefined,
                  heartbeat :: reference() | undefined,
                  sequence :: integer() | undefined,
-                 session_id :: binary() | undefined
+                 session_id :: binary() | undefined,
+                 log :: file:io_device()
                }).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -45,16 +46,18 @@ handle_cast(connect, State) ->
     ApiServer = discordant_sup:get_api_server(),
     BinGateway = discord_api:get_gateway(ApiServer),
     "wss://" ++ Gateway = binary:bin_to_list(BinGateway),
-    logger:info("connecting to discord gateway~n"),
+    logger:info("connecting to discord gateway"),
     {ok, ConnPid} = gun:open(Gateway, 443, #{protocols => [http]}),
     {ok, _Protocol} = gun:await_up(ConnPid),
     MRef = monitor(process, ConnPid),
     gun:ws_upgrade(ConnPid, "/"),
     receive
         {gun_upgrade, ConnPid, _StreamRef, [<<"websocket">>], _Headers} ->
+            {ok, Log} = file:open("events.log", [append, {encoding, unicode}]),
             {noreply,
              State#state{url=Gateway,
-                         connection=#connection{pid=ConnPid, ref=MRef}}};
+                         connection=#connection{pid=ConnPid, ref=MRef},
+                         log=Log}};
         {gun_response, ConnPid, _StreamRef, _Fin, _Status, _Headers} ->
             {stop, ws_upgrade_failed, State};
         {gun_error, ConnPid, _StreamRef, Reason} ->
@@ -64,11 +67,11 @@ handle_cast(connect, State) ->
     end;
 handle_cast(heartbeat, S=#state{connection=#connection{pid=Pid},
                                 sequence=Seq}) ->
-    logger:info("sending heartbeat~n"),
+    logger:info("sending heartbeat"),
     send_message(Pid, 1, Seq),
     {noreply, S};
 handle_cast(identify, S=#state{connection=#connection{pid=Pid}, token=Token}) ->
-    logger:info("sending identify~n"),
+    logger:info("sending identify"),
     send_message(Pid, 2, #{<<"token">> => Token,
                            <<"properties">> => #{
                                <<"$os">> => <<"beam">>,
@@ -79,9 +82,10 @@ handle_cast(identify, S=#state{connection=#connection{pid=Pid}, token=Token}) ->
     {noreply, S}.
 
 handle_info({gun_ws, ConnPid, _StreamRef, {text, Msg}},
-            S=#state{connection=#connection{pid=ConnPid}}) ->
+            S=#state{connection=#connection{pid=ConnPid}, log=Log}) ->
     Json = jsone:decode(Msg),
     logger:debug("message recevied: ~p", [Json]),
+    ok = file:write(Log, [Msg, "\n"]),
     NewState = handle_ws_message(Json, S),
     {noreply, NewState}.
 
@@ -101,11 +105,14 @@ handle_ws_message_(0, _Msg, State) ->
     State;
 handle_ws_message_(10, #{<<"d">> := #{<<"heartbeat_interval">> := IV}},
                    State) ->
+    if State#state.heartbeat =:= undefined -> ok;
+       true -> discord_heartbeat:remove_heartbeat(State#state.heartbeat)
+    end,
     Ref = discord_heartbeat:create_heartbeat(IV, self()),
     discord_gateway:heartbeat(self()),
     State#state{heartbeat=Ref};
 handle_ws_message_(11, _Msg, State) ->
-    logger:info("recieved heartbeat ack"),
+    logger:info("received heartbeat ack"),
     % TODO make sure we receive an ack between heartbeats
     case State#state.session_id of
         undefined -> gen_server:cast(self(), identify);
--- src/discord_heartbeat.erl
+++ src/discord_heartbeat.erl
@@ -1,8 +1,12 @@
 -module(discord_heartbeat).
 
--export([create_heartbeat/2]).
+-export([create_heartbeat/2, remove_heartbeat/1]).
 
 create_heartbeat(Interval, Pid) ->
     {ok, TRef} = timer:apply_interval(Interval, discord_gateway, heartbeat,
                                       [Pid]),
     TRef.
+
+remove_heartbeat(Ref) ->
+    {ok, cancel} = timer:cancel(Ref),
+    ok.
